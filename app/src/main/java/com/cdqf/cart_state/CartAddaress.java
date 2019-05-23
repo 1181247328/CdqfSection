cae31e2ca58f3ed6f45987126a5fe744b3201a40
WpSmile <179292126@qq.com>
2019-05-23 17:07:33 +0800

    2019/05/23

--- app/src/main/java/com/cdqf/cart_state/CartAddaress.java
+++ app/src/main/java/com/cdqf/cart_state/CartAddaress.java
@@ -9,7 +9,7 @@ public class CartAddaress {
     //public static final String ADDRESS = "http://192.168.31.9";
 
     //线上
-    public static final String ADDRESS = "http://testapi.baodi520.com";
+    public static final String ADDRESS = "https://testapi.baodi520.com";
 
     //线上
 //    public static final String ADDRESS = "https://tgapi.baodi520.com";
