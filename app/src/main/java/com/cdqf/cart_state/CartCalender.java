43c41b986a6dc576412b55e6dca6e8ec8383e336
WpSmile <179292126@qq.com>
2019-08-05 14:34:35 +0800

    2019/08/05 提交项目

--- app/src/main/java/com/cdqf/cart_state/CartCalender.java
+++ app/src/main/java/com/cdqf/cart_state/CartCalender.java
@@ -173,4 +173,34 @@ public class CartCalender {
         }
         return null;
     }
+
+    /**
+     * 获取当前月有多少天
+     */
+    public int getMonthNumber(int year, int month) {
+        int day = 0;
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            day = 29;
+        } else {
+            day = 28;
+        }
+        switch (month) {
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                return 31;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                return 30;
+            case 2:
+                return day;
+        }
+        return 0;
+    }
 }
